859cc1cca3dee069735676f5852611716e6dc1c1
HyunSeongLee <devhyun05@gmail.com>
2024-02-23 10:44:20 -0500

    solution added

--- Two Sum (2024-02-23)/Two Sum (Hyun).js
+++ Two Sum (2024-02-23)/Two Sum (Hyun).js
@@ -4,17 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numMap = new Map(); 
-
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i]; 
-
-        if (numMap.has(complement)) {
-            return [numMap.get(complement), i]; 
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]; 
+            }
         }
-
-        numMap.set(nums[i], i);
     }
-
-    return -1; // if no matching number set exists in the array 
 };
